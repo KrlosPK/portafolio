f2b394a5ed6f4207106fbca5af47e794e9ca8597
dofejutipro <102671583+dofejuti10@users.noreply.github.com>
2023-07-10 20:47:30 -0400

    Primer Comit

---
+++ index.js
@@ -0,0 +1,54 @@
+//para animacioncon scroll//
+window.sr = ScrollReveal();
+
+  sr.reveal('.scroll-title', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.scroll-skills', {
+    duration: 3000,
+    origin: 'left',
+    distance: '200px'
+  });
+
+  sr.reveal('.scroll-hobbies', {
+    duration: 2000,
+    origin: 'left',
+    distance: '-200px'
+  });
+
+  sr.reveal('.scroll-title', {
+    rotate: {
+        x: 0,
+        y: 180, 
+        duration: 4000,
+    }
+  });
+
+  sr.reveal('.scroll-skills', {
+    rotate: {
+        x: 0,
+        y: 180, 
+        duration: 4000,
+    }
+  });
+
+  sr.reveal('.scroll-contacto', {
+    duration: 3000,
+    origin: 'left',
+    distance: '200px'
+  });
+
+  sr.reveal('.footer', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+  });
+
+
+
+  
+
+
